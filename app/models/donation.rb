d8bf6fe7c6bc4bae2e356dcf90971473d2345f38
Caio Andrade <caiotdandrade@gmail.com>
2019-05-19 12:13:59 -0300

    Adds form validation
    - Validates title, description, detail, phone and e-mail
    - adds format validation form phone : must be in the format
    (dd)dddddddd or (dd)dddddddd (2 digits followed by 8 digits or
        9 digits), where d is a number
    - adds format validation for zip code form : must be in format
        ddddd-ddd (d must be a number)
    - adds format validation for e-mail form : must be in format
        string@string.com or string@string.com.br (where string is any string
        containing numbers or characters)
    
    Signed-off-by: Caio Andrade <caiotdandrade@gmail.com>

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,4 +2,10 @@ class Donation < ApplicationRecord
   has_one_attached :image
   validates :title, presence: true
   validates :description, presence: true
+  validates :detail, presence: true
+  validates :phone,presence: true,numericality: true
+  validates :email, presence: true
+  validates_format_of :phone, :with => /^\(\d\d\)\d{9}$|^\(\d\d\)\d{8}$/, multiline: true
+  validates_format_of :zip, with: /^\d{5}-\d{3}$/, multiline: true
+  validates_format_of :email, with: /.+@.+\.com(\.br)?/
 end

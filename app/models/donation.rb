b325fa549661115fc59fc4dfa4852415d388644e
Caio Andrade <caiotdandrade@gmail.com>
2019-05-19 12:22:08 -0300

    Fixes validation on phone

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -5,7 +5,7 @@ class Donation < ApplicationRecord
   validates :detail, presence: true
   validates :phone,presence: true,numericality: true
   validates :email, presence: true
-  validates_format_of :phone, :with => /^\(\d\d\)\d{9}$|^\(\d\d\)\d{8}$/, multiline: true
+  validates_format_of :phone, :with => /^(\d\d)\d{9}$|^\(\d\d\)\d{8}$/, multiline: true
   validates_format_of :zip, with: /^\d{5}-\d{3}$/, multiline: true
   validates_format_of :email, with: /.+@.+\.com(\.br)?/
 end

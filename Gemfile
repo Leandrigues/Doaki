c1e854d8f4f115fef36f2cbdad22a09eb3695b76
Édio Cerati Neto <edioceratineto@gmail.com>
2019-05-23 11:04:17 -0300

    Add 'mail' gem to validate mail addresses accordingly to RFC2822 standards

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+# Email validation gem for the forms
+gem 'mail'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,11 +1,12 @@
 class Donation < ApplicationRecord
+  include EmailValidatable
+
   has_one_attached :image
   validates :title, presence: true
   validates :description, presence: true
   validates :detail, presence: true
   validates :phone,presence: true
-  validates :email, presence: true
+  validates :email, presence: true, email: true
   validates_format_of :phone, :with => /^(\d\d)\d{9}$|^\(\d\d\)\d{8}$/, multiline: true
   validates_format_of :zip, with: /^\d{5}-\d{3}$/, multiline: true
-  validates_format_of :email, with: /.+@.+\.com(\.br)?/
 end

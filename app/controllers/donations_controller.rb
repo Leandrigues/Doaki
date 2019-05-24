e5139bf0c75abcbb2484151f8b1b98c2173650b5
Leandro Rodrigues <rodriguesleandro@usp.br>
2019-05-24 16:38:23 -0300

    Add multiple images upload feature

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -69,6 +69,6 @@ class DonationsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def donation_params
-      params.require(:donation).permit(:title, :description, :street, :detail, :zip, :phone, :email, :image)
+      params.require(:donation).permit(:title, :description, :street, :detail, :zip, :phone, :email, images: [])
     end
 end

13daa119d1ebf133ba69e8bc9562f759f38b6167
Leandro Rodrigues <rodriguesleandro@usp.br>
2019-06-11 16:45:55 -0300

    Create users, sessions, add gem to encrypt password and modify navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   get 'about', to: 'pages#about'
   root 'pages#home'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :donations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

6c94a292bb7eb04d6e3cb8e7eb54f6837072f675
Leandro Rodrigues <rodriguesleandro@usp.br>
2019-06-18 16:16:28 -0300

    Validates images extesion

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -8,4 +8,18 @@ class Donation < ApplicationRecord
   validates :email, presence: true, email: true
   validates_format_of :phone, :with => /^(\d\d)\d{9}$|^\(\d\d\)\d{8}$/, multiline: true
   validates_format_of :zip, with: /^\d{5}-\d{3}$/, multiline: true
+  validate :image_type
+
+  private
+  def image_type
+    if images.attached?
+      images.each do |image|
+        if !image.content_type.in?(%('image/jpeg image/png'))
+          errors.add(:images, ": Formato de imagem inválido. Permitido: JPEG ou PNG.")
+        end
+      end
+    end
+  end
+
 end
+

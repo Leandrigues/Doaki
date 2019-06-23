1a414a248481a020b7fb53c09732ab2cd7866ded
viniciusgpereira <vini.gpereira2014@gmail.com>
2019-06-23 19:34:50 -0300

    Changes errors style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   def current_user
-    if session[:user_id]
+    if session[:user_id] && User.all.length != 0
       @current_user ||= User.find(session[:user_id])
     else
       @current_user = nil
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
 	has_many :comments
 	has_secure_password
     validates :email, presence: true, uniqueness: true
+    validates :name, presence: true
 end
